ff0a909733f037dc50afcf21173a4b42c8bbcde4
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-11-02 14:20:29 -0500

    Update selectGTFfile_streamlit.py

--- selectGTFfile_streamlit.py
+++ selectGTFfile_streamlit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 27 14:16:50 2023
@@ -41,16 +41,15 @@ selected_version = st.selectbox("Select GTF Version", list(gtf_urls.keys()))
 st.write("You selected:", selected_version)
 
 gtf_url = gtf_urls.get(selected_version)
-if gtf_url:
-    gtf_data = download_gtf_file(gtf_url)
-    if gtf_data:
-        # Display the first 1000 characters of the file as an example
-        st.write("First 1000 characters of the GTF file:")
-        st.text(gtf_data[:1000])
-    else:
-        st.write("Failed to download the GTF file. Please check the URL.")
+if gtf_url:gtf_data = download_gtf_file(gtf_url)
+    if gtf_data:
+        #Display the first 1000 characters of the file as an example
+        st.write("First 1000 characters of the GTF file:")
+        st.text(gtf_data[:1000])
+    else:
+        st.write("Failed to download the GTF file. Please check the URL.")
 else:
-    st.write("Invalid GTF version selected.")
+    st.write("Invalid GTF version selected.")
 
 # Empty list
 selected_genes = []
@@ -59,32 +58,30 @@ github_file_url = 'https://raw.githubusercontent.com/monnieb92/selectGTFfile_str
 
 # Split GTF data into lines if gtf_data is not None
 if gtf_data:
-    gtf_lines = gtf_data.splitlines()
+    gtf_lines = gtf_data.splitlines()
 
-    # Read gene IDs from the uploaded file
-    if gene_list is not None:
-        gene_list_df = pd.read_csv(gene_list, sep="\t")
-        gene_id_list = gene_list_df[gene_id_name].tolist()
-    else:
-        response = requests.get(github_file_url)
-        gene_list_df = pd.read_csv(StringIO(response.text), sep="\t")
-        gene_id_list = gene_list_df[gene_id_name].tolist()
-    
-    # Loop through the GTF data
-    for line in gtf_lines:
-        elements = line.split("\t")
-        if len elements < 9:
-            continue
-        attributes = elements[8]
-        if gene_id_name in attributes:
-            gene_id = attributes.split(gene_id_name)[1].split('";')[0].strip(' "')
-            if gene_id in gene_id_list:
-                selected_genes.append(line)
+    # Read gene IDs from the uploaded file
+    if gene_list is not None:
+        gene_list_df = pd.read_csv(gene_list, sep="\t")
+        gene_id_list = gene_list_df[gene_id_name].tolist()
+    else:
+        response = requests.get(github_file_url)
+        gene_list_df = pd.read_csv(StringIO(response.text), sep="\t")
+        gene_id_list = gene_list_df[gene_id_name].tolist()
+        
+    # Loop through the GTF data
+    for line in gtf_lines:
+        elements = line.split("\t")
+        if len elements < 9:
+            continue
+        attributes = elements[8]
+        if gene_id_name in attributes:
+            gene_id = attributes.split(gene_id_name)[1].split('";')[0].strip(' "')
+            if gene_id in gene_id_list:
+                selected_genes.append(line)
 
 # Create a button for downloading the selected GTF
 if st.button("Download Selected GTF"):
     with open(downloadpath, "w") as new_gtf_file:
         for entry in selected_genes:
             new_gtf_file.write(entry)
-    
-    st.write(f"Download the selected GTF file: [downloadpath]({downloadpath})")
