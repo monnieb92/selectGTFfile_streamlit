b1e552a3e2da96acb427b2f8f6a75eeeb5383575
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-11-02 13:47:41 -0500

    Update selectGTFfile_streamlit.py

--- selectGTFfile_streamlit.py
+++ selectGTFfile_streamlit.py
@@ -6,39 +6,52 @@ Created on Fri Oct 27 14:16:50 2023
 @author: monicabomber
 """
 
-import streamlit as st 
+import streamlit as st
 import requests
 import pandas as pd
 from io import StringIO
 
-gene_list = st.file_uploader("Upload a tab deliminated file of your selected gene list")
-gene_id_name = st.text_area("How you are aligning the files, by gene_id", value = "gene_id")
-downloadpath = st.text_area("The filename to download the gtf file", value = "download.gtf")
+gene_list = st.file_uploader("Upload a tab delimited file of your selected gene list")
+gene_id_name = st.text_area("How you are aligning the files, by gene_id", value="gene_id")
+downloadpath = st.text_area("The filename to download the GTF file", value="download.gtf")
 
-gtf_data=None
+gtf_data = None
 
 # Function to download GTF file from the selected URL
 def download_gtf_file(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 # Dictionary mapping GTF versions to their respective URLs
 gtf_urls = {
-    "hg38": "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.ncbiRefSeq.gtf.gz",
-    "hg19": "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.ncbiRefSeq.gtf.gz",
-    "mm10": "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/genes/mm10.ncbiRefSeq.gtf.gz",
-    "mm39": "https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/genes/mm39.ncbiRefSeq.gtf.gz"
+    "hg38": "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.ncbiRefSeq.gtf.gz",
+    "hg19": "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.ncbiRefSeq.gtf.gz",
+    "mm10": "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/genes/mm10.ncbiRefSeq.gtf.gz",
+    "mm39": "https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/genes/mm39.ncbiRefSeq.gtf.gz"
 }
 
 # Create a dropdown to select the GTF version
 selected_version = st.selectbox("Select GTF Version", list(gtf_urls.keys()))
 
 # Display the selected version
-st.write("You selected:", selected_version)   
-#Empty list 
+st.write("You selected:", selected_version)
+
+gtf_url = gtf_urls.get(selected_version)
+if gtf_url:
+    gtf_data = download_gtf_file(gtf_url)
+    if gtf_data:
+        # Display the first 1000 characters of the file as an example
+        st.write("First 1000 characters of the GTF file:")
+        st.text(gtf_data[:1000])
+    else:
+        st.write("Failed to download the GTF file. Please check the URL.")
+else:
+    st.write("Invalid GTF version selected.")
+
+# Empty list
 selected_genes = []
 
 github_file_url = 'https://raw.githubusercontent.com/monnieb92/selectGTFfile_streamlit/main/selectgenelist.txt'
@@ -59,7 +72,7 @@ if gtf_data:
     # Loop through the GTF data
     for line in gtf_lines:
         elements = line.split("\t")
-        if len(elements) < 9:
+        if len elements < 9:
             continue
         attributes = elements[8]
         if gene_id_name in attributes:
@@ -67,6 +80,10 @@ if gtf_data:
             if gene_id in gene_id_list:
                 selected_genes.append(line)
 
-with open(downloadpath, "w") as new_gtf_file:
-    for entry in selected_genes:
-        new_gtf_file.write(entry)
+# Create a button for downloading the selected GTF
+if st.button("Download Selected GTF"):
+    with open(downloadpath, "w") as new_gtf_file:
+        for entry in selected_genes:
+            new_gtf_file.write(entry)
+    
+    st.write(f"Download the selected GTF file: [downloadpath]({downloadpath})")
